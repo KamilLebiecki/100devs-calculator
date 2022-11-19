7bce50fe5e4244e0bd44516a4867749f1985172b
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-11-19 18:00:07 +0100

    main.js edit, added Fortnite styling

--- README.md
+++ README.md
@@ -1,26 +1,17 @@
-# 🔢 #100Devs Push Project001: Calculator
+# Simple Calculator
 
-### Goal: Build a Simple Calculator using JS OOP best practices
+Simple tool to do basic arithmetic stuff, theme with Fortnite game for my kids.
 
-### What it should look like:
+**Link to project:** http://recruiters-love-seeing-live-demos.com/
 
-![Calculator](calculator.jpg)
+**Tech used:** HTML, CSS, vanilla JavaScript
 
-### How to submit your code for review:
+## Optimizations to do:
 
-- Fork and clone this repo
-- Create a new branch called answer
-- Checkout answer branch
-- Push to your fork
-- Issue a pull request
-- Your pull request description should contain the following:
-  - (1 to 5 no 3) I completed the challenge
-  - (1 to 5 no 3) I feel good about my code
-  - Anything specific on which you want feedback!
+- Fix second decimal sign in the input
+- Other themes for the calculator
+- Button/switcher for themes
 
-Example:
-```
-I completed the challenge: 5
-I feel good about my code: 4
-I'm not sure if my constructors are setup cleanly...
-```
+## Lessons Learned:
+
+Eval method requires to have working with clean input provided by the end-user.
--- main.js
+++ main.js
@@ -5,7 +5,60 @@ keys.addEventListener('click', (event) => {
 	if (!target.matches('button')) {
 		return;
 	} else {
-		//pass to parse method;
-		console.log(event);
+		calculator.parseInput(value);
 	}
 });
+
+const calculator = {
+	displayText: '0',
+	prevTotal: null,
+
+	parseInput(value) {
+		switch (value) {
+			case '=':
+				this.calcAnswer(this.displayText);
+				break;
+			case 'AC':
+				this.clearAll();
+				break;
+			case '.':
+				if (this.displayText === 0) {
+					this.addText('0.');
+				} else {
+					this.addText(value);
+				}
+				break;
+			default:
+				this.addText(value);
+				break;
+		}
+	},
+	addText(value) {
+		if (this.displayText === '0') {
+			this.displayText = '';
+		} else if (this.prevTotal !== null) {
+			this.prevTotal = null;
+		}
+		if (isNaN(+value) && isNaN(+this.displayText)) {
+			if (isNaN(this.displayText.slice(-1))) {
+				return;
+			}
+		}
+		this.displayText += value;
+		this.outputText(this.displayText);
+	},
+	outputText(text) {
+		document.querySelector('.calculator-screen').value = text;
+	},
+
+	calcAnswer(equation) {
+		let result = Function('return ' + equation)();
+		this.outputText(result);
+	},
+
+	clearAll() {
+		(this.displayText = '0'),
+			(this.prevTotal = null),
+			this.outputText(this.displayText);
+	},
+};
